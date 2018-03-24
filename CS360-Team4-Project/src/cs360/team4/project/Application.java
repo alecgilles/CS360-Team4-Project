b141729052c676b1ecf0ef23aafb29ae17229537
Cooper <Cooper@Cooper-PC.fios-router.home>
2018-03-24 13:29:59 -0400

    Adding school and school's file reader. NOTE: poorly documented

--- CS360-Team4-Project/src/cs360/team4/project/Application.java
+++ CS360-Team4-Project/src/cs360/team4/project/Application.java
@@ -3,9 +3,9 @@ package cs360.team4.project;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// josh was here
-
+		SchoolReader sr = new SchoolReader();
+		sr.readFile("School_File.csv");
+		System.out.println(sr.schoolArr.toString());
 	}
 
 }
---
+++ CS360-Team4-Project/src/cs360/team4/project/School.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+
+/**
+ * @author Cooper
+ *
+ */
+public class School {
+	String name;
+	int id;
+	
+	// null constructor
+	public School(){
+		this.name = null;
+		this.id = -1;
+	}
+	
+	// paramater constructor
+	public School (String tName, int tId){
+		this.name = tName;
+		this.id = tId;	
+		
+	}
+	
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("School [");
+		builder.append(name);
+		builder.append(",  id: ");
+		builder.append(id);
+		builder.append("]\n");
+		return builder.toString();
+	}
+
+}
