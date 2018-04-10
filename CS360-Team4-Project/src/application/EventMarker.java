ce361f2c6573557fe45d3fe022ff3e9072dd73ec
Alec Gilles <alecgilles@gmail.com>
2018-04-10 15:08:49 -0400

    Prettied up travel time and changed selected event marker to green

--- CS360-Team4-Project/src/application/EventMarker.java
+++ CS360-Team4-Project/src/application/EventMarker.java
@@ -6,15 +6,16 @@ import com.lynden.gmapsfx.util.MarkerImageFactory;
 
 public class EventMarker extends Marker {
 	private int eventId;
-	
+
 	public EventMarker(int eventId, MarkerOptions markerOptions) {
 		super(markerOptions);
-		
-		markerOptions.icon(MarkerImageFactory.createMarkerImage("/view/img/school_icon_red.png", "png").replace("(", "").replace(")", ""));
+
+		markerOptions.icon(MarkerImageFactory.createMarkerImage("/view/img/school_icon_red.png", "png").replace("(", "")
+				.replace(")", ""));
 		this.setOptions(markerOptions);
 		this.eventId = eventId;
 	}
-	
+
 	public int getEventId() {
 		return eventId;
 	}
